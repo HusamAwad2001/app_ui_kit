f879d68c889bb11db0b5b559f8a390411ebce123
HusamAwad2001 <www.hosamawad2016@gmail.com>
2023-10-04 13:49:46 +0300

    GestureDetector on text field

--- lib/src/app_text_field.dart
+++ lib/src/app_text_field.dart
@@ -51,71 +51,75 @@ class AppTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      obscureText: isPassword,
-      enabled: isEnabled,
+    return GestureDetector(
       onTap: (isEnabled ?? true) ? () {} : onTap,
-      maxLines: isPassword ? 1 : maxLines,
-      minLines: isPassword ? 1 : minLines,
-      maxLength: maxLength,
-      controller: textEditingController ?? TextEditingController(),
-      textInputAction: textInputAction,
-      keyboardType: textInputType,
-      onTapOutside: (_) {
-        FocusManager.instance.primaryFocus?.unfocus();
-      },
-      style: TextStyle(color: textColor ?? Colors.black),
-      cursorColor: hintColor ?? Theme.of(context).primaryColor,
-      decoration: InputDecoration(
-          hintStyle: hintStyle ?? TextStyle(color: hintColor ?? Colors.grey),
-          hintText: label,
-          counter: const SizedBox.shrink(),
-          enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
-              borderSide: BorderSide(color: borderColor ?? Colors.transparent)),
-          focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
-              borderSide: BorderSide(color: Theme.of(context).primaryColor)),
-          disabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
-              borderSide: BorderSide(color: borderColor ?? Colors.transparent)),
-          filled: true,
-          prefixIcon: prefix == null
-              ? const SizedBox()
-              : Container(
-                  width: kMinInteractiveDimension,
-                  height: kMinInteractiveDimension,
-                  alignment: Alignment.center,
-                  child: prefix,
-                ),
-          prefixIconConstraints: prefix == null
-              ? BoxConstraints(minWidth: vPadding)
-              : Theme.of(context).visualDensity.effectiveConstraints(
-                    const BoxConstraints(
-                      minWidth: kMinInteractiveDimension,
-                      minHeight: kMinInteractiveDimension,
-                    ),
+      child: TextField(
+        obscureText: isPassword,
+        enabled: isEnabled,
+        maxLines: isPassword ? 1 : maxLines,
+        minLines: isPassword ? 1 : minLines,
+        maxLength: maxLength,
+        controller: textEditingController ?? TextEditingController(),
+        textInputAction: textInputAction,
+        keyboardType: textInputType,
+        onTapOutside: (_) {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+        style: TextStyle(color: textColor ?? Colors.black),
+        cursorColor: hintColor ?? Theme.of(context).primaryColor,
+        decoration: InputDecoration(
+            hintStyle: hintStyle ?? TextStyle(color: hintColor ?? Colors.grey),
+            hintText: label,
+            counter: const SizedBox.shrink(),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+                borderSide:
+                    BorderSide(color: borderColor ?? Colors.transparent)),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+                borderSide: BorderSide(color: Theme.of(context).primaryColor)),
+            disabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+                borderSide:
+                    BorderSide(color: borderColor ?? Colors.transparent)),
+            filled: true,
+            prefixIcon: prefix == null
+                ? const SizedBox()
+                : Container(
+                    width: kMinInteractiveDimension,
+                    height: kMinInteractiveDimension,
+                    alignment: Alignment.center,
+                    child: prefix,
                   ),
-          suffixIcon: suffix == null
-              ? const SizedBox()
-              : Container(
-                  width: kMinInteractiveDimension,
-                  height: kMinInteractiveDimension,
-                  alignment: Alignment.center,
-                  child: suffix,
-                ),
-          suffixIconConstraints: suffix == null
-              ? BoxConstraints(minWidth: vPadding)
-              : Theme.of(context).visualDensity.effectiveConstraints(
-                    const BoxConstraints(
-                      minWidth: kMinInteractiveDimension,
-                      minHeight: kMinInteractiveDimension,
+            prefixIconConstraints: prefix == null
+                ? BoxConstraints(minWidth: vPadding)
+                : Theme.of(context).visualDensity.effectiveConstraints(
+                      const BoxConstraints(
+                        minWidth: kMinInteractiveDimension,
+                        minHeight: kMinInteractiveDimension,
+                      ),
                     ),
+            suffixIcon: suffix == null
+                ? const SizedBox()
+                : Container(
+                    width: kMinInteractiveDimension,
+                    height: kMinInteractiveDimension,
+                    alignment: Alignment.center,
+                    child: suffix,
                   ),
-          contentPadding:
-              EdgeInsets.symmetric(vertical: vPadding, horizontal: hPadding),
-          fillColor: backgroundColor ?? Colors.white,
-          isDense: true),
+            suffixIconConstraints: suffix == null
+                ? BoxConstraints(minWidth: vPadding)
+                : Theme.of(context).visualDensity.effectiveConstraints(
+                      const BoxConstraints(
+                        minWidth: kMinInteractiveDimension,
+                        minHeight: kMinInteractiveDimension,
+                      ),
+                    ),
+            contentPadding:
+                EdgeInsets.symmetric(vertical: vPadding, horizontal: hPadding),
+            fillColor: backgroundColor ?? Colors.white,
+            isDense: true),
+      ),
     );
   }
 }
